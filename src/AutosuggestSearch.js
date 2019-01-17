5eedf5f6c5a2f4d6fd022fdb2028774407a5f89e
Zachary Geier <zdgeier@gmail.com>
2019-01-17 17:05:28 -0500

    removed bundle and reformatted autosuggest

--- src/AutosuggestSearch.js
+++ src/AutosuggestSearch.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Autosuggest from 'react-autosuggest';
-import match from 'autosuggest-highlight/match';
-import parse from 'autosuggest-highlight/parse';
 import TextField from '@material-ui/core/TextField';
 import Paper from '@material-ui/core/Paper';
 import MenuItem from '@material-ui/core/MenuItem';
@@ -29,9 +27,7 @@ function renderInputComponent(inputProps) {
 }
 
 function getSuggestionValue(suggestion) {
-    //console.dir(suggestion);
-    //return `${suggestion.shortDesc} (id:${suggestion.id}) maintained by ${suggestion.maintainers}`;
-    return suggestion.id;
+    return suggestion.label;
 }
 
 const styles = theme => ({
@@ -67,14 +63,9 @@ class AutosuggestSearch extends React.Component {
         suggestions: [],
     };
 
-    renderSuggestion = (suggestion, { query, isHighlighted }) => {
-        const matches = match(suggestion.label, query);
-        const parts = parse(suggestion.label, matches);
-
+    renderSuggestion = suggestion => {
         return (
             <MenuItem
-                selected={isHighlighted}
-                component="div"
                 onClick={(event) => {
                     console.dir(event.target.textContent)
                     fetch(`http://libx.org/editions/config/${event.target.textContent}`)
@@ -85,24 +76,11 @@ class AutosuggestSearch extends React.Component {
                             console.log(json.revisions.live.config);
                             this.props.fetchEdition(json.revisions.live.config);
                         })
-                }}
-            >
-                <div>
-                    {parts.map((part, index) => {
-                        return part.highlight ? (
-                            <span key={String(index)} style={{ fontWeight: 500 }}>
-                {part.text}
-              </span>
-                        ) : (
-                            <strong key={String(index)} style={{ fontWeight: 300 }}>
-                                {part.text}
-                            </strong>
-                        );
-                    })}
-                </div>
+                }}>
+                {suggestion.label}
             </MenuItem>
         );
-    }
+    };
 
     handleSuggestionsFetchRequested = ({ value }) => {
         fetch(`http://libx.org/editions/search?q=${value}`)
